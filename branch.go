d0fe6b62e383725a193c52095f8723d9d8baacc2
Lac <laclys@126.com>
2018-11-17 11:59:34 +0800

    added

--- branch.go
+++ branch.go
@@ -1,27 +1,27 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-)
+// import (
+// 	"fmt"
+// 	"io/ioutil"
+// )
 
-func grade(score int) string {
-	g := ""
-	switch {
-	case score < 0 || score > 100:
-		panic(
-			fmt.Sprintf("wrong score: %d", score))
-	case score < 60:
-		g = "F"
-	case score < 80:
-		g = "C"
-	case score < 90:
-		g = "B"
-	case score <= 100:
-		g = "A"
-	}
-	return g
-}
+// func grade(score int) string {
+// 	g := ""
+// 	switch {
+// 	case score < 0 || score > 100:
+// 		panic(
+// 			fmt.Sprintf("wrong score: %d", score))
+// 	case score < 60:
+// 		g = "F"
+// 	case score < 80:
+// 		g = "C"
+// 	case score < 90:
+// 		g = "B"
+// 	case score <= 100:
+// 		g = "A"
+// 	}
+// 	return g
+// }
 
 // func main() {
 // 	const filename = "abc.txt"
